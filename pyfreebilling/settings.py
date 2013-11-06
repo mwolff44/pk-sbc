6222ddb87546f126d32d36b00a2458d90f91b829
mwolff <mwolff@MacBook-Air-de-Mathias.local>
2013-11-06 13:49:44 +0100

    Updates

--- pyfreebilling/settings.py
+++ pyfreebilling/settings.py
@@ -1,3 +1,19 @@
+# Copyright 2013 Mathias WOLFF
+# This file is part of pyfreebilling.
+# 
+# pyfreebilling is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# pyfreebilling is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with pyfreebilling.  If not, see <http://www.gnu.org/licenses/>
+
 import os
 # Django settings for pyfreebilling project.
  
@@ -5,7 +21,7 @@ import os
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-PFB_VERSION = 'v1.1beta'
+PFB_VERSION = 'v1.2beta'
 
 SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.dirname(SETTINGS_DIR)
