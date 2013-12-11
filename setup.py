099268da56e116793808178191f34079182d947c
mwolff <mathiaswolff@mac.com>
2013-12-11 19:27:41 +0100

    Add test settings

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pyfreebilling',
+      version='1.23',
+      description='Softswitch billing app.',
+      author='Mathias WOLFF',
+      author_email='mathiaswolff@mac.com',
+      url='https://bitbucket.org/mwolff/pyfreebilling/',
+      packages=find_packages(),
+      license='License :: GPL',
+
+      # Enable django-setuptest
+      test_suite='setuptest.setuptest.SetupTestSuite',
+      tests_require=(
+        'django-setuptest',
+        # Required by django-setuptools on Python 2.6
+        'argparse'
+      ),
+)
