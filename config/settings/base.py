dc5d81d32ca93085060c1780722c37dd6705706f
Mathias WOLFF <mathias@celea.org>
2020-01-15 11:30:23 +0100

    Remove deprecated password hasher

--- config/settings/base.py
+++ config/settings/base.py
@@ -127,7 +127,6 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
