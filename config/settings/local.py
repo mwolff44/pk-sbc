3f31dd6b478b6b7e46c0728f13cadb5626b526b1
mwolff44 <mathias@celea.org>
2020-03-23 18:36:08 +0100

    corrections

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default='9WHEm3uAIeAFOmg3L7uknpEKjOGo98rMXutHrjcLdI1Vfa2MfypaWCM5k5hvi0SA',
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
