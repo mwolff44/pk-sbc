692b2f45a1fee44bcc9355bcdd58db05e556fdf2
Mathias <mathias@celea.org>
2020-05-16 15:12:59 +0200

    add drf

--- pyfreebilling/users/tests/test_views.py
+++ pyfreebilling/users/tests/test_views.py
@@ -1,11 +1,17 @@
 import pytest
-from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+from django.http.response import Http404
 from django.test import RequestFactory
 
-from pyfreebilling.users.views import UserRedirectView, UserUpdateView
+from pyfreebilling.users.models import User
+from pyfreebilling.users.tests.factories import UserFactory
+from pyfreebilling.users.views import (
+    UserRedirectView,
+    UserUpdateView,
+    user_detail_view,
+)
 
 pytestmark = pytest.mark.django_db
-
 
 class TestUserUpdateView:
     """
@@ -16,22 +22,18 @@ class TestUserUpdateView:
         https://github.com/pytest-dev/pytest-django/pull/258
     """
 
-    def test_get_success_url(
-        self, user: settings.AUTH_USER_MODEL, request_factory: RequestFactory
-    ):
+    def test_get_success_url(self, user: User, rf: RequestFactory):
         view = UserUpdateView()
-        request = request_factory.get("/fake-url/")
+        request = rf.get("/fake-url/")
         request.user = user
 
         view.request = request
 
         assert view.get_success_url() == f"/users/{user.username}/"
 
-    def test_get_object(
-        self, user: settings.AUTH_USER_MODEL, request_factory: RequestFactory
-    ):
+    def test_get_object(self, user: User, rf: RequestFactory):
         view = UserUpdateView()
-        request = request_factory.get("/fake-url/")
+        request = rf.get("/fake-url/")
         request.user = user
 
         view.request = request
@@ -40,14 +42,37 @@ class TestUserUpdateView:
 
 
 class TestUserRedirectView:
-
-    def test_get_redirect_url(
-        self, user: settings.AUTH_USER_MODEL, request_factory: RequestFactory
-    ):
+    def test_get_redirect_url(self, user: User, rf: RequestFactory):
         view = UserRedirectView()
-        request = request_factory.get("/fake-url")
+        request = rf.get("/fake-url")
         request.user = user
 
         view.request = request
 
         assert view.get_redirect_url() == f"/users/{user.username}/"
+
+
+class TestUserDetailView:
+    def test_authenticated(self, user: User, rf: RequestFactory):
+        request = rf.get("/fake-url/")
+        request.user = UserFactory()
+
+        response = user_detail_view(request, username=user.username)
+
+        assert response.status_code == 200
+
+    def test_not_authenticated(self, user: User, rf: RequestFactory):
+        request = rf.get("/fake-url/")
+        request.user = AnonymousUser()  # type: ignore
+
+        response = user_detail_view(request, username=user.username)
+
+        assert response.status_code == 302
+        assert response.url == "/accounts/login/?next=/fake-url/"
+
+    def test_case_sensitivity(self, rf: RequestFactory):
+        request = rf.get("/fake-url/")
+        request.user = UserFactory(username="UserName")
+
+        with pytest.raises(Http404):
+            user_detail_view(request, username="username")
