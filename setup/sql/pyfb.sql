b59dad43a7e22249da2070ae7e3ed85cb0b3e011
Mathias WOLFF <mathias@celea.org>
2020-09-24 11:03:47 +0200

    change uuid generation

--- setup/sql/pyfb.sql
+++ setup/sql/pyfb.sql
@@ -1,8 +1,8 @@
--- FUNCTION: public.create_cdr2()
+-- FUNCTION: public.create_cdr()
 
 -- DROP FUNCTION public.create_cdr();
 
-CREATE FUNCTION public.create_cdr2()
+CREATE FUNCTION public.create_cdr()
     RETURNS trigger
     LANGUAGE 'plpgsql'
     COST 100
@@ -168,14 +168,14 @@ END IF;
 	IF NEW.term_customer!= 0 and NEW.orig_provider !=0 AND NEW.term_provider = 0 AND NEW.orig_customer = 0 then
 
 	INSERT INTO pyfb_reporting_cdr(id, customer_ip, aleg_uuid, caller_number, callee_number, start_time, answered_time, end_time, duration, sip_code, sip_reason, sip_charge_info, sip_rtp_rxstat, sip_rtp_txstat, kamailio_server, direction, callee_destination_id, caller_destination_id, customer_id, provider_id, media_server_id, call_class, cdr_acc_id, billsec, costsec, total_sell, total_cost, cost_rate, rate, read_codec, write_codec, sip_user_agent, hangup_disposition, rated)
-      VALUES (uuid_generate_v4(), '', NEW.callid, NEW.e164_caller, NEW.e164_called, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, NEW.direction, NEW.called_destination, NEW.caller_destination, NEW.term_customer, NEW.orig_provider, NEW.media_server, NEW.leg_a_class, new.ID, t_billsec, o_costsec, t_total_sell, o_total_cost, o_cost_rate, t_rate, '', '', '', '', current_timestamp);
+      VALUES (gen_random_uuid(), '', NEW.callid, NEW.e164_caller, NEW.e164_called, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, NEW.direction, NEW.called_destination, NEW.caller_destination, NEW.term_customer, NEW.orig_provider, NEW.media_server, NEW.leg_a_class, new.ID, t_billsec, o_costsec, t_total_sell, o_total_cost, o_cost_rate, t_rate, '', '', '', '', current_timestamp);
 	UPDATE acc_cdrs SET processed = current_timestamp WHERE id = NEW.id;
 	
 	-- outbound call delivered to provider
 	ELSIF NEW.orig_customer!= 0 and NEW.term_provider !=0 AND NEW.orig_provider = 0 AND NEW.term_customer = 0then
 
 	INSERT INTO pyfb_reporting_cdr(id, customer_ip, aleg_uuid, caller_number, callee_number, start_time, answered_time, end_time, duration, sip_code, sip_reason, sip_charge_info, sip_rtp_rxstat, sip_rtp_txstat, kamailio_server, direction, callee_destination_id, caller_destination_id, customer_id, provider_id, media_server_id, call_class, cdr_acc_id, billsec, costsec, total_sell, total_cost, cost_rate, rate, read_codec, write_codec, sip_user_agent, hangup_disposition, rated)
-      VALUES (uuid_generate_v4(), '', NEW.callid, NEW.e164_caller, NEW.e164_called, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, NEW.direction, NEW.called_destination, NEW.caller_destination, NEW.orig_customer, NEW.term_provider, NEW.media_server, NEW.leg_a_class, new.ID, o_billsec, t_costsec, o_total_sell, t_total_cost, t_cost_rate, o_rate, '', '', '', '', current_timestamp);
+      VALUES (gen_random_uuid(), '', NEW.callid, NEW.e164_caller, NEW.e164_called, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, NEW.direction, NEW.called_destination, NEW.caller_destination, NEW.orig_customer, NEW.term_provider, NEW.media_server, NEW.leg_a_class, new.ID, o_billsec, t_costsec, o_total_sell, t_total_cost, t_cost_rate, o_rate, '', '', '', '', current_timestamp);
 	UPDATE acc_cdrs SET processed = current_timestamp WHERE id = NEW.id;
 	
 	-- inbound call forwarded to outside number
@@ -183,8 +183,8 @@ END IF;
 	ELSIF NEW.orig_provider!= 0 and NEW.term_provider !=0 then
 
 	INSERT INTO pyfb_reporting_cdr(id, customer_ip, aleg_uuid, caller_number, callee_number, start_time, answered_time, end_time, duration, sip_code, sip_reason, sip_charge_info, sip_rtp_rxstat, sip_rtp_txstat, kamailio_server, direction, callee_destination_id, caller_destination_id, customer_id, provider_id, media_server_id, call_class, cdr_acc_id, billsec, costsec, total_sell, total_cost, cost_rate, rate, read_codec, write_codec, sip_user_agent, hangup_disposition, rated)
-      VALUES (uuid_generate_v4(), '', NEW.callid, NEW.e164_caller, NEW.called_did, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, NEW.direction, NEW.did_destination, NEW.caller_destination, NEW.term_customer, NEW.orig_provider, NEW.media_server, NEW.leg_a_class, new.ID, t_billsec, o_costsec, t_total_sell, o_total_cost, o_cost_rate, t_rate, '', '', '', '', current_timestamp),
-	         (uuid_generate_v4(), '', NEW.callid, NEW.called_did, NEW.e164_called, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, 'outbound', NEW.called_destination, NEW.did_destination, NEW.term_customer, NEW.term_provider, NEW.media_server, NEW.leg_b_class, new.ID, o_billsec, t_costsec, o_total_sell, t_total_cost, t_cost_rate, o_rate, '', '', '', '', current_timestamp);
+      VALUES (gen_random_uuid(), '', NEW.callid, NEW.e164_caller, NEW.called_did, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, NEW.direction, NEW.did_destination, NEW.caller_destination, NEW.term_customer, NEW.orig_provider, NEW.media_server, NEW.leg_a_class, new.ID, t_billsec, o_costsec, t_total_sell, o_total_cost, o_cost_rate, t_rate, '', '', '', '', current_timestamp),
+	         (gen_random_uuid(), '', NEW.callid, NEW.called_did, NEW.e164_called, NEW.start_time, NEW.answered_time, NEW.end_time, NEW.duration, coalesce(NEW.sip_code,''), coalesce(NEW.sip_reason, ''), coalesce(NEW.sip_charge_info, ''), coalesce(NEW.sip_rtp_rxstat, '0'), coalesce(NEW.sip_rtp_txstat, '0'), NEW.kamailio_server, 'outbound', NEW.called_destination, NEW.did_destination, NEW.term_customer, NEW.term_provider, NEW.media_server, NEW.leg_b_class, new.ID, o_billsec, t_costsec, o_total_sell, t_total_cost, t_cost_rate, o_rate, '', '', '', '', current_timestamp);
 	UPDATE acc_cdrs SET processed = current_timestamp WHERE id = NEW.id;
 	
 	END IF;
@@ -192,12 +192,12 @@ END IF;
 	-- UPDATE BALANCE
 	UPDATE pyfb_company co SET customer_balance = co.customer_balance -o_total_sell FROM pyfb_customer cu WHERE co.id = cu.company_id AND cu.id = NEW.orig_customer AND co.customer_balance_update = True;
 	UPDATE pyfb_company co SET customer_balance = co.customer_balance -t_total_sell FROM pyfb_customer cu WHERE co.id = cu.company_id AND cu.id = NEW.term_customer AND co.customer_balance_update = True;
-	UPDATE pyfb_company co SET carrier_balance = co.carrier_balance -o_total_cost FROM pyfb_provider pr WHERE co.id = pr.company_id AND cu.id = NEW.orig_provider AND co.provider_balance_update = True;
-	UPDATE pyfb_company co SET carrier_balance = co.carrier_balance -t_total_cost FROM pyfb_provider pr WHERE co.id = pr.company_id AND cu.id = NEW.term_provider AND co.provider_balance_update = True;
+	UPDATE pyfb_company co SET supplier_balance = co.supplier_balance -o_total_cost FROM pyfb_provider pr WHERE co.id = pr.company_id AND pr.id = NEW.orig_provider AND co.provider_balance_update = True;
+	UPDATE pyfb_company co SET supplier_balance = co.supplier_balance -t_total_cost FROM pyfb_provider pr WHERE co.id = pr.company_id AND pr.id = NEW.term_provider AND co.provider_balance_update = True;
 	
 	RETURN NULL;
 END;
 $BODY$;
 
-ALTER FUNCTION public.create_cdr2()
+ALTER FUNCTION public.create_cdr()
     OWNER TO pyfreebilling;
